a5fd56a08cbaeb72b96c5380abd660b0113756e7
mohammedshakleya <shaki2632@gmail.com>
2023-07-04 19:57:36 +0200

    test: Added tests for validator.

---
+++ util/validator/validator_test.go
@@ -0,0 +1,61 @@
+package validator_test
+
+import (
+	"testing"
+
+	"github.com/bwc00/strv-go-newsletter-shakleya-mohammed/util/validator"
+)
+
+type testCase struct {
+	name     string
+	input    interface{}
+	expected string
+}
+
+var tests = []*testCase{
+	{
+		name: `required`,
+		input: struct {
+			Title string `json:"title" form:"required"`
+		}{},
+		expected: "title is a required field",
+	},
+	{
+		name: `max`,
+		input: struct {
+			Course string `json:"course" form:"max=7"`
+		}{Course: "CS-0001."},
+		expected: "course must be a maximum of 7 in length",
+	},
+	{
+		name: `email`,
+		input: struct {
+			Email string `json:"email" form:"email"`
+		}{Email: "shaki2632.com"},
+		expected: "email must be a valid Email",
+	},
+	{
+		name: `alpha_space`,
+		input: struct {
+			Name string `json:"name" form:"alpha_space"`
+		}{Name: "Some Name 2"},
+		expected: "name can only contain alphabetic and space characters",
+	},
+}
+
+func TestToErrResponse(t *testing.T) {
+	vr := validator.New()
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			err := vr.Struct(tc.input)
+			if errResp := validator.ToErrResponse(err); errResp == nil || len(errResp.Errors) != 1 {
+				t.Fatalf(`Expected:"{[%v]}", Got:"%v"`, tc.expected, errResp)
+			} else if errResp.Errors[0] != tc.expected {
+				t.Fatalf(`Expected:"%v", Got:"%v"`, tc.expected, errResp.Errors[0])
+			}
+		})
+	}
+}
